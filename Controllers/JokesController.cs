9f549c95877bb2365688753513479f35309e17eb
Paluch Alex-Sebastian <alexpaluch43@gmail.com>
2024-02-20 19:50:04 +0200

    add show search form

--- Controllers/JokesController.cs
+++ Controllers/JokesController.cs
@@ -25,6 +25,12 @@ namespace JokesWebApp.Controllers
             return View(await _context.Joke.ToListAsync());
         }
 
+        // GET: Jokes/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
